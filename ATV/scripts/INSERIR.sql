914fd69fcaf8504e40dc2f22d54de6497e0a1957
Jullya Castro <96029889+jully4@users.noreply.github.com>
2025-09-04 10:33:19 -0400

    adicionando pasta

---
+++ ATV/scripts/INSERIR.sql
@@ -0,0 +1,12 @@
+INSERT INTO filme(titulo,diretor,genero,ano) VALUES ("perderam o nemo","jose","animacao",2025);
+INSERT INTO filme(titulo,diretor,genero,ano) VALUES ("Procurando nemo","riva","animacao",2005);
+INSERT INTO filme(titulo,diretor,genero,ano) VALUES ("Acharam o nemo","jova","cancao",1990);
+
+INSERT INTO livro(titulo,autor,genero,editora,ano) VALUES ("talvez","jairo","horror","UFMT",2012);
+INSERT INTO livro(titulo,autor,genero,editora,ano) VALUES ("nemo","riva jose","romance","MTUF",1001);
+INSERT INTO livro(titulo,autor,genero,editora,ano) VALUES ("amor","bruno","romance","TMFU",2005);
+
+INSERT INTO musica(titulo,artista,ano) VALUES ("jaronimos","roberto",2020);
+INSERT INTO musica(titulo,artista,ano) VALUES ("jaros","robhet",1990);
+INSERT INTO musica(titulo,artista,ano) VALUES ("alunosas","berto",1990);
+INSERT INTO musica(titulo,artista,ano) VALUES ("jardim","roto",2025);
